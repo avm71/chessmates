4b3ff3b3a8d60d4242096a6015d0ca95d6e5c89d
Anmol Mynam <anmolmynam@gmail.com>
2022-03-29 20:08:30 -0400

    pending changes

--- contracts/Chessmates.sol
+++ contracts/Chessmates.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: UNLICNESED
 pragma solidity >=0.7.3 < 0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -5,11 +6,27 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Chessmates is ERC721 {
     using Counters for Counters.Counter;
-    Counters.Counter private tokenID;
-    string uriPrefix = "";
-    
+    using Strings for uint256;
 
-    constructor() ERC721("ChessMates", "CHESS"){}
+    Counters.Counter private tokenID;
+
+    // metadata
+    string public uriPrefix = "";
+    string public uriSuffix = ".json";
+    string public hiddenMetaDataUri;
+
+    // cost/supply/maxMint
+    uint256 public cost = 0.08 ether;
+    uint256 public supply = 10000;
+    uint256 public maxMint = 5;
+
+    // NFT reveal/hidden
+    bool public reveal = false;
+    bool public hide = true;
+
+    constructor() ERC721("ChessMates", "CHESS"){
+        setHiddenMetadataUri("ipfs://__CID__/hidden.json");
+    }
 
     function mintNFT(address recipient, string memory _tokenURI) public returns(uint256) {
         tokenID.increment();
